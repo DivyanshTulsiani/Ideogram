4f9763a83d0da4747695d42418ec61408f44301d
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-09-06 13:52:39 +0530

    added middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,6 +1,8 @@
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { Request, Response, NextFunction } from "express"
 import dotenv from "dotenv"
+import { UserModel } from "../models/db"
+import { email } from "zod"
 
 dotenv.config()
 
@@ -11,19 +13,39 @@ const JWT_SECRET = process.env.JWT_SECRET
 //an entire express app can be considered basically a chain of middlewares only this funciton additionaly has 
 //a next() object which basically call the next function in line if the call is succesfull
 
-async function authmiddleware(req: Request, res: Response, next: NextFunction) {
+export default async function authmiddleware(req: Request, res: Response, next: NextFunction) {
   const token = req.headers.authorization;
 
   if (token) {
-    if (!JWT_SECRET) {
-      throw new Error("Jwt secret is not present");
+    try{
+      if (!JWT_SECRET) {
+        throw new Error("Jwt secret is not present");
+      }
+      const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+      if (decoded.email) {
+        let userfound = await UserModel.findOne({email: decoded.email})
+        if(userfound){
+
+          next();
+        }
+        else{
+          res.status(404).json({
+            message: "Unable to find the requested user"
+          })
+        }
+      }
     }
-    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
-    if (decoded.email) {
-      
+    catch(e){
+      res.status(404).json({
+        message: "Some error occured",
+        error: e
+      })
     }
   }
   else {
-
+    res.status(500).json({
+      message: "Token not found"
+    })
   }
 }
