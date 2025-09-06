d5b2f38d12f5950db1d95b1602685d9b75d15413
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-09-06 16:19:05 +0530

    setting up endpoints for micrsoervcie

--- server/src/routes/generate.ts
+++ server/src/routes/generate.ts
@@ -1,6 +1,19 @@
 import express from "express"
 import { Router } from "express"
-
+import { Request,Response } from "express";
+import { ContentPromptModel } from "../models/db";
+import  authmiddleware  from "../middleware/auth"
 
 
 const router = Router();
+
+
+
+router.post("/uploadpdf",authmiddleware,async (req: Request,res: Response) =>{
+
+})
+
+
+router.post("/generate",authmiddleware,async (req: Request,res: Response) =>{
+  
+})
