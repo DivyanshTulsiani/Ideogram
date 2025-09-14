1c9c8ead8477f045bd81e4bcda4935aabb783393
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-09-14 16:35:22 +0530

    added oauth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+
+
 // import App from './App.tsx'
 
 import App from './dagre.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
