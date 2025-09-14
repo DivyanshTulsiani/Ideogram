42ff3e384b63d2297b97c89948f3bae16970da81
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-09-15 00:45:02 +0530

    fixed vite env

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,15 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 
 
-// import App from './App.tsx'
+import App from './App.tsx'
 
-import App from './dagre.tsx'
+// import App from './dagre.tsx'
 
 createRoot(document.getElementById('root')!).render(
+  
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
