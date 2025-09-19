b84f2f154f500f868ddb47c58886d8f690f6065b
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-09-19 23:56:01 +0530

    added provider for state management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,67 @@
-
+import { useState, createContext, useEffect, useContext } from 'react';
 import '@xyflow/react/dist/style.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from './pages/authpage';
 import Reactflow from './pages/reactflow'
+import {
+    type Node,
+    type Edge
+} from '@xyflow/react';
 
+interface FlowProviderProps {
+    children: React.ReactNode
+}
+
+interface FlowContextType {
+    initialEdges: Edge[],
+    initialNodes: Node[],
+    SetinitialEdges: (edges: Edge[]) => void;
+    SetinitialNodes: (nodes: Node[]) => void;
+}
+
+export const FlowContext = createContext<FlowContextType | undefined>(undefined);
+
+export const FlowProvider = ({ children }: FlowProviderProps) => {
+    const [initialEdges, SetinitialEdges] = useState<Edge[]>([])
+    const [initialNodes, SetinitialNodes] = useState<Node[]>([])
+
+    return (
+        <>
+            <FlowContext.Provider value={{
+                initialEdges: initialEdges,
+                SetinitialEdges: SetinitialEdges,
+                initialNodes: initialNodes,
+                SetinitialNodes: SetinitialNodes
+            }}>
+                {children}
+            </FlowContext.Provider>
+        </>
+    )
+}
+
+export const useFlowContext = () => {
+    const context = useContext(FlowContext);
+    
+    if (!context) {
+        throw new Error('useFlowContext must be used within a FlowProvider');
+    }
+    
+    return context;
+};
 
 
 export default function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/">
-                <Route path="/" element={<AuthPage/>}/>
-                <Route path="/flow" element={<Reactflow/>}/>
-            </Route>
-        </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <FlowProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/">
+                        <Route path="/" element={<AuthPage />} />
+                        <Route path="/flow" element={<Reactflow />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </FlowProvider>
+    );
 }
 
