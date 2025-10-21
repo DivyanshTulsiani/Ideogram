549b05cc2116b4d269f19234bbb821a5de5cf368
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-10-21 23:30:54 +0530

    added navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { useState,useEffect } from "react";
+
+
+const Navbar = () => {
+  return(
+    <>
+      <div className=" h-[3.5rem] w-full bg-white shadow-sm">
+        h
+      </div>
+    </>
+  )
+}
+
+
+export default Navbar
