8aaa81b05ab266efc862d41de4784b24ffa40648
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-10-25 01:58:19 +0530

    sidebar added w/o usemd for now

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,111 +1,128 @@
-import { useReactFlow, type XYPosition } from '@xyflow/react';
-import { useCallback, useState } from 'react';
-import { type OnDropAction, useDnD, useDnDPosition } from '../hooks/useDnd';
-import { memo } from 'react';
+import { motion, AnimatePresence } from "framer-motion"
+import { useMedia } from "../hooks/useMedia";
+import { useEffect } from "react";
 
-// This is a simple ID generator for the nodes.
-// You can customize this to use your own ID generation logic.
-let id = 0;
-const getId = () => `dndnode_${id++}`;
+interface SidebarProps {
+  SidebarOpen: boolean;
+  SetSidebar: (SidebarOpen: boolean) => void;
+}
 
-function Sidebar() {
-  const { onDragStart, isDragging } = useDnD();
-  // The type of the node that is being dragged.
-  const [type, setType] = useState<string | null>(null);
+const Sidebar = (props: SidebarProps) => {
 
-  const { setNodes } = useReactFlow();
+  const IsMd = useMedia("(min-width: 768px)")
 
-  const createAddNewNode = useCallback(
-    (nodeType: string, label: string, shape: string): OnDropAction => {
-      return ({ position }: { position: XYPosition }) => {
-        // Here, we create a new node and add it to the flow.
-        // You can customize the behavior of what happens when a node is dropped on the flow here.
-        const newNode = {
-          id: getId(),
-          type: nodeType,
-          position,
-          data: { label: `${label}`, shape: `${shape}` },
-        };
+  console.log("IsMd:", IsMd);
 
-        setNodes((nds) => nds.concat(newNode));
-        setType(null);
-      };
-    },
-    [setNodes, setType],
-  );
+  // useEffect(()=>{
+  //   if(IsMd){
+  //     props.SetSidebar(true)
+  //   }
+  //   else{
+  //     props.SetSidebar(false)
+  //   }
+
+  // },[IsMd])
 
   return (
-    <>
-      {/* The ghost node will be rendered at pointer position when dragging. */}
-      {isDragging && <DragGhost type={type} />}
-      <aside className='shadow-sm'> 
-
-        <section>
-
-          <div
-            className="dndnode input"
-            onPointerDown={(event) => {
-              setType('edit');
-              onDragStart(event, createAddNewNode('edit', 'hehhhllo', 'rectangle'));
-            }}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square"><rect width="18" height="18" x="3" y="3" rx="2"></rect></svg>
+    <AnimatePresence>
+      <motion.div
+        initial={{ width: props.SidebarOpen ? "16rem" : "3.5rem" }}
+        animate={{ width: props.SidebarOpen ? "16rem" : "3.5rem" }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className="bg-[#f7f7f7] h-screen border-r-1 border-gray-200"
+      >
+        {props.SidebarOpen ? (<div className="flex flex-col">
+          {/* this is top of sidebar */}
+          <div className="flex justify-between px-5 py-2 items-center h-[3.5rem] border-b-1 border-gray-200">
+            <div className="flex justify-center items-center gap-2">
+              <div className="bg-[#515dfe] font-inter flex items-center justify-center w-8 h-8 text-[#f1f6ff] rounded-md">
+                Id
+              </div>
+              <div className="font-medium font-inter text-md">
+                Ideogram
+              </div>
+            </div>
+            <div>
+              j
+            </div>
           </div>
-          <div
-            className="dndnode"
-            onPointerDown={(event) => {
-              setType('edit');
-              onDragStart(event, createAddNewNode('edit', 'hello', 'diamond'));
-            }}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-diamond"><path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z"></path></svg>
+
+          {/* input comp */}
+          <div className="px-3">
+            <div className="flex justify-center gap-1 h-[2.5rem] items-center mt-3 bg-white shadow-sm rounded-lg border-1 border-gray-300">
+              <div className="font-gray-200 flex-none">
+                
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="#64748b" xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true"><path d="M14.0857 8.74999C14.0857 5.80355 11.6972 3.41503 8.75073 3.41503C5.80429 3.41503 3.41577 5.80355 3.41577 8.74999C3.41577 11.6964 5.80429 14.085 8.75073 14.085C11.6972 14.085 14.0857 11.6964 14.0857 8.74999ZM15.4158 8.74999C15.4158 10.3539 14.848 11.8245 13.9041 12.9746L13.9705 13.0303L16.9705 16.0303L17.0564 16.1338C17.2269 16.3919 17.1977 16.7434 16.9705 16.9707C16.7432 17.1975 16.3925 17.226 16.1345 17.0557L16.03 16.9707L13.03 13.9707L12.9753 13.9033C11.8253 14.8472 10.3547 15.415 8.75073 15.415C5.06975 15.415 2.08569 12.431 2.08569 8.74999C2.08569 5.06901 5.06975 2.08495 8.75073 2.08495C12.4317 2.08495 15.4158 5.06901 15.4158 8.74999Z"></path></svg>
+              </div>
+              <input placeholder="Search" className="font-inter bg-white rounded-lg outline-none w-[10rem] " />
+              <div className="flex items-center gap-1 bg-gray-100 px-1 py-0.5 text-gray-500 text-sm rounded-md">
+                <span className="font-medium text-xs">⌘</span>
+                <span className="font-medium text-xs">K</span>
+              </div>
+            </div>
+          </div>
+
+          {/* now these are our siebar items */}
+
+          <div className="flex flex-col font-inter px-3 mt-4 gap-2 border-b-1 border-gray-200">
+            <div className="flex justify-left hover:bg-[#efefef] px-2 rounded-lg h-[2.3rem] text-gray-600 gap-3 items-center">
+              <div className="font-gray-600 flex-none">
+              <svg width="17" height="17" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274" stroke="#64748b" stroke-width="1.5" stroke-linecap="round"></path> <path d="M15 18H9" stroke="#64748b" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+              </div>
+              <div className="text-md">
+                Home
+              </div>
+            </div>
+
+            <div className="flex justify-left hover:bg-[#efefef] px-2 rounded-lg h-[2.3rem]  text-gray-600 gap-3 items-center">
+              <div className="font-gray-600 flex-none">
+              <svg width="17" height="17" viewBox="0 0 20 20" fill="#64748b" xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true"><path d="M2.6687 11.333V8.66699C2.6687 7.74455 2.66841 7.01205 2.71655 6.42285C2.76533 5.82612 2.86699 5.31731 3.10425 4.85156L3.25854 4.57617C3.64272 3.94975 4.19392 3.43995 4.85229 3.10449L5.02905 3.02149C5.44666 2.84233 5.90133 2.75849 6.42358 2.71582C7.01272 2.66769 7.74445 2.66797 8.66675 2.66797H9.16675C9.53393 2.66797 9.83165 2.96586 9.83179 3.33301C9.83179 3.70028 9.53402 3.99805 9.16675 3.99805H8.66675C7.7226 3.99805 7.05438 3.99834 6.53198 4.04102C6.14611 4.07254 5.87277 4.12568 5.65601 4.20313L5.45581 4.28906C5.01645 4.51293 4.64872 4.85345 4.39233 5.27149L4.28979 5.45508C4.16388 5.7022 4.08381 6.01663 4.04175 6.53125C3.99906 7.05373 3.99878 7.7226 3.99878 8.66699V11.333C3.99878 12.2774 3.99906 12.9463 4.04175 13.4688C4.08381 13.9833 4.16389 14.2978 4.28979 14.5449L4.39233 14.7285C4.64871 15.1465 5.01648 15.4871 5.45581 15.7109L5.65601 15.7969C5.87276 15.8743 6.14614 15.9265 6.53198 15.958C7.05439 16.0007 7.72256 16.002 8.66675 16.002H11.3337C12.2779 16.002 12.9461 16.0007 13.4685 15.958C13.9829 15.916 14.2976 15.8367 14.5447 15.7109L14.7292 15.6074C15.147 15.3511 15.4879 14.9841 15.7117 14.5449L15.7976 14.3447C15.8751 14.128 15.9272 13.8546 15.9587 13.4688C16.0014 12.9463 16.0017 12.2774 16.0017 11.333V10.833C16.0018 10.466 16.2997 10.1681 16.6667 10.168C17.0339 10.168 17.3316 10.4659 17.3318 10.833V11.333C17.3318 12.2555 17.3331 12.9879 17.2849 13.5771C17.2422 14.0993 17.1584 14.5541 16.9792 14.9717L16.8962 15.1484C16.5609 15.8066 16.0507 16.3571 15.4246 16.7412L15.1492 16.8955C14.6833 17.1329 14.1739 17.2354 13.5769 17.2842C12.9878 17.3323 12.256 17.332 11.3337 17.332H8.66675C7.74446 17.332 7.01271 17.3323 6.42358 17.2842C5.90135 17.2415 5.44665 17.1577 5.02905 16.9785L4.85229 16.8955C4.19396 16.5601 3.64271 16.0502 3.25854 15.4238L3.10425 15.1484C2.86697 14.6827 2.76534 14.1739 2.71655 13.5771C2.66841 12.9879 2.6687 12.2555 2.6687 11.333ZM13.4646 3.11328C14.4201 2.334 15.8288 2.38969 16.7195 3.28027L16.8865 3.46485C17.6141 4.35685 17.6143 5.64423 16.8865 6.53613L16.7195 6.7207L11.6726 11.7686C11.1373 12.3039 10.4624 12.6746 9.72827 12.8408L9.41089 12.8994L7.59351 13.1582C7.38637 13.1877 7.17701 13.1187 7.02905 12.9707C6.88112 12.8227 6.81199 12.6134 6.84155 12.4063L7.10132 10.5898L7.15991 10.2715C7.3262 9.53749 7.69692 8.86241 8.23218 8.32715L13.2791 3.28027L13.4646 3.11328ZM15.7791 4.2207C15.3753 3.81702 14.7366 3.79124 14.3035 4.14453L14.2195 4.2207L9.17261 9.26856C8.81541 9.62578 8.56774 10.0756 8.45679 10.5654L8.41772 10.7773L8.28296 11.7158L9.22241 11.582L9.43433 11.543C9.92426 11.432 10.3749 11.1844 10.7322 10.8271L15.7791 5.78027L15.8552 5.69629C16.185 5.29194 16.1852 4.708 15.8552 4.30371L15.7791 4.2207Z"></path></svg>
+              </div>
+              <div className="text-md">
+                New Project
+              </div>
+            </div>
+
+            <div className="flex justify-left hover:bg-[#efefef] px-2 rounded-lg h-[2.3rem] text-gray-600 gap-3 mb-4 items-center">
+              <div className="font-gray-600 flex-none">
+              <svg width="17" height="17" viewBox="0 0 24 24" fill="#64748b" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M13 2.5V5C13 7.35702 13 8.53553 13.7322 9.26777C14.4645 10 15.643 10 18 10H22" stroke="#1C274C" stroke-width="1.5"></path> <path d="M8.14595 13.0225C8.52594 12.3408 8.71594 12 9 12C9.28406 12 9.47406 12.3408 9.85405 13.0225L9.95236 13.1989C10.0603 13.3926 10.1143 13.4894 10.1985 13.5533C10.2827 13.6172 10.3875 13.641 10.5972 13.6884L10.7881 13.7316C11.526 13.8986 11.895 13.982 11.9828 14.2643C12.0706 14.5466 11.819 14.8407 11.316 15.429L11.1858 15.5812C11.0429 15.7483 10.9714 15.8319 10.9392 15.9353C10.9071 16.0387 10.9179 16.1502 10.9395 16.3733L10.9592 16.5763C11.0352 17.3612 11.0733 17.7536 10.8435 17.9281C10.6136 18.1025 10.2682 17.9435 9.5773 17.6254L9.39855 17.5431C9.20222 17.4527 9.10406 17.4075 9 17.4075C8.89594 17.4075 8.79778 17.4527 8.60145 17.5431L8.4227 17.6254C7.7318 17.9435 7.38635 18.1025 7.15654 17.9281C6.92674 17.7536 6.96476 17.3612 7.04082 16.5763L7.0605 16.3733C7.08211 16.1502 7.09291 16.0387 7.06076 15.9353C7.0286 15.8319 6.95713 15.7483 6.81418 15.5812L6.68403 15.429C6.18097 14.8407 5.92945 14.5466 6.01723 14.2643C6.10501 13.982 6.47396 13.8986 7.21186 13.7316L7.40277 13.6884C7.61246 13.641 7.7173 13.6172 7.80148 13.5533C7.88567 13.4894 7.93966 13.3926 8.04764 13.1989L8.14595 13.0225Z" stroke="#1C274C" stroke-width="1.5"></path> <path d="M2.75 10C2.75 9.58579 2.41421 9.25 2 9.25C1.58579 9.25 1.25 9.58579 1.25 10H2.75ZM21.25 14C21.25 14.4142 21.5858 14.75 22 14.75C22.4142 14.75 22.75 14.4142 22.75 14H21.25ZM15.3929 4.05365L14.8912 4.61112L15.3929 4.05365ZM19.3517 7.61654L18.85 8.17402L19.3517 7.61654ZM21.654 10.1541L20.9689 10.4592V10.4592L21.654 10.1541ZM3.17157 20.8284L3.7019 20.2981H3.7019L3.17157 20.8284ZM20.8284 20.8284L20.2981 20.2981L20.2981 20.2981L20.8284 20.8284ZM1.35509 5.92658C1.31455 6.33881 1.61585 6.70585 2.02807 6.7464C2.4403 6.78695 2.80734 6.48564 2.84789 6.07342L1.35509 5.92658ZM22.6449 18.0734C22.6855 17.6612 22.3841 17.2941 21.9719 17.2536C21.5597 17.2131 21.1927 17.5144 21.1521 17.9266L22.6449 18.0734ZM14 21.25H10V22.75H14V21.25ZM2.75 14V10H1.25V14H2.75ZM21.25 13.5629V14H22.75V13.5629H21.25ZM14.8912 4.61112L18.85 8.17402L19.8534 7.05907L15.8947 3.49618L14.8912 4.61112ZM22.75 13.5629C22.75 11.8745 22.7651 10.8055 22.3391 9.84897L20.9689 10.4592C21.2349 11.0565 21.25 11.742 21.25 13.5629H22.75ZM18.85 8.17402C20.2034 9.3921 20.7029 9.86199 20.9689 10.4592L22.3391 9.84897C21.9131 8.89241 21.1084 8.18853 19.8534 7.05907L18.85 8.17402ZM10.0298 2.75C11.6116 2.75 12.2085 2.76158 12.7405 2.96573L13.2779 1.5653C12.4261 1.23842 11.498 1.25 10.0298 1.25V2.75ZM15.8947 3.49618C14.8087 2.51878 14.1297 1.89214 13.2779 1.5653L12.7405 2.96573C13.2727 3.16993 13.7215 3.55836 14.8912 4.61112L15.8947 3.49618ZM10 21.25C8.09318 21.25 6.73851 21.2484 5.71085 21.1102C4.70476 20.975 4.12511 20.7213 3.7019 20.2981L2.64124 21.3588C3.38961 22.1071 4.33855 22.4392 5.51098 22.5969C6.66182 22.7516 8.13558 22.75 10 22.75V21.25ZM1.25 14C1.25 15.8644 1.24841 17.3382 1.40313 18.489C1.56076 19.6614 1.89288 20.6104 2.64124 21.3588L3.7019 20.2981C3.27869 19.8749 3.02502 19.2952 2.88976 18.2892C2.75159 17.2615 2.75 15.9068 2.75 14H1.25ZM14 22.75C15.8644 22.75 17.3382 22.7516 18.489 22.5969C19.6614 22.4392 20.6104 22.1071 21.3588 21.3588L20.2981 20.2981C19.8749 20.7213 19.2952 20.975 18.2892 21.1102C17.2615 21.2484 15.9068 21.25 14 21.25V22.75ZM10.0298 1.25C8.15538 1.25 6.67442 1.24842 5.51887 1.40307C4.34232 1.56054 3.39019 1.8923 2.64124 2.64124L3.7019 3.7019C4.12453 3.27928 4.70596 3.02525 5.71785 2.88982C6.75075 2.75158 8.11311 2.75 10.0298 2.75V1.25ZM2.84789 6.07342C2.96931 4.83905 3.23045 4.17335 3.7019 3.7019L2.64124 2.64124C1.80633 3.47616 1.48944 4.56072 1.35509 5.92658L2.84789 6.07342ZM21.1521 17.9266C21.0307 19.1609 20.7695 19.8266 20.2981 20.2981L21.3588 21.3588C22.1937 20.5238 22.5106 19.4393 22.6449 18.0734L21.1521 17.9266Z" fill="#1C274C"></path> </g></svg>
+              </div>
+              <div className="text-md">
+                Favourites
+              </div>
+            </div>
 
           </div>
-          <div
-            className="dndnode output"
-            onPointerDown={(event) => {
-              setType('edit');
-              onDragStart(event, createAddNewNode('edit', 'hello', 'oval'));
-            }}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle"><circle cx="12" cy="12" r="10"></circle></svg>
-          </div>
-        </section>
 
-        <section className='border-t-1 border-black'>
-          <div className='mt-3 flex justify-center items-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
-          </div>
-        </section>
+          
+        </div>) :
+          <div>
+            jjj
+          </div>}
 
-      </aside>
-
-    </>
-  );
+      </motion.div>
+    </AnimatePresence>
+  )
 }
 
-interface DragGhostProps {
-  type: string | null;
+interface SidebarItemProps{
+  icon: React.ReactNode;
+  context: string;
+
 }
 
-// The DragGhost component is used to display a ghost node when dragging a node into the flow.
-export function DragGhost({ type }: DragGhostProps) {
-  const { position } = useDnDPosition();
-  // console.log('DragGhost position:', position);
+const SidebarItem = (props: SidebarItemProps) => {
 
-  if (!position) return null;
-
-  return (
-    <div
-      className={`dndnode ghostnode ${type}`}
-      style={{
-        transform: `translate(${position.x}px, ${position.y}px) translate(-50%, -50%)`
-      }}
-    >
-      {type && `${type.charAt(0).toUpperCase() + type.slice(1)} Node`}
-    </div>
-  );
 }
 
+export default Sidebar
 
-export default memo(Sidebar)
+
+{/* <>  
+{props.SidebarOpen ? (<div className="bg-[#f7f7f7] h-screen w-[16rem] border-r-1 border-gray-200">
+  hiiiii
+
+</div>) 
+: <div className="bg-[#f7f7f7] h-screen w-[4rem] border-r-1 border-gray-200">
+  </div>}
+</> */}
---
+++ client/src/components/SidebarEdit.tsx
@@ -0,0 +1,111 @@
+import { useReactFlow, type XYPosition } from '@xyflow/react';
+import { useCallback, useState } from 'react';
+import { type OnDropAction, useDnD, useDnDPosition } from '../hooks/useDnd';
+import { memo } from 'react';
+
+// This is a simple ID generator for the nodes.
+// You can customize this to use your own ID generation logic.
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+function SidebarEdit() {
+  const { onDragStart, isDragging } = useDnD();
+  // The type of the node that is being dragged.
+  const [type, setType] = useState<string | null>(null);
+
+  const { setNodes } = useReactFlow();
+
+  const createAddNewNode = useCallback(
+    (nodeType: string, label: string, shape: string): OnDropAction => {
+      return ({ position }: { position: XYPosition }) => {
+        // Here, we create a new node and add it to the flow.
+        // You can customize the behavior of what happens when a node is dropped on the flow here.
+        const newNode = {
+          id: getId(),
+          type: nodeType,
+          position,
+          data: { label: `${label}`, shape: `${shape}` },
+        };
+
+        setNodes((nds) => nds.concat(newNode));
+        setType(null);
+      };
+    },
+    [setNodes, setType],
+  );
+
+  return (
+    <>
+      {/* The ghost node will be rendered at pointer position when dragging. */}
+      {isDragging && <DragGhost type={type} />}
+      <aside className='shadow-sm'> 
+
+        <section>
+
+          <div
+            className="dndnode input"
+            onPointerDown={(event) => {
+              setType('edit');
+              onDragStart(event, createAddNewNode('edit', 'hehhhllo', 'rectangle'));
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square"><rect width="18" height="18" x="3" y="3" rx="2"></rect></svg>
+          </div>
+          <div
+            className="dndnode"
+            onPointerDown={(event) => {
+              setType('edit');
+              onDragStart(event, createAddNewNode('edit', 'hello', 'diamond'));
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-diamond"><path d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41l-7.59-7.59a2.41 2.41 0 0 0-3.41 0Z"></path></svg>
+
+          </div>
+          <div
+            className="dndnode output"
+            onPointerDown={(event) => {
+              setType('edit');
+              onDragStart(event, createAddNewNode('edit', 'hello', 'oval'));
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle"><circle cx="12" cy="12" r="10"></circle></svg>
+          </div>
+        </section>
+
+        <section className='border-t-1 border-black'>
+          <div className='mt-3 flex justify-center items-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
+          </div>
+        </section>
+
+      </aside>
+
+    </>
+  );
+}
+
+interface DragGhostProps {
+  type: string | null;
+}
+
+// The DragGhost component is used to display a ghost node when dragging a node into the flow.
+export function DragGhost({ type }: DragGhostProps) {
+  const { position } = useDnDPosition();
+  // console.log('DragGhost position:', position);
+
+  if (!position) return null;
+
+  return (
+    <div
+      className={`dndnode ghostnode ${type}`}
+      style={{
+        transform: `translate(${position.x}px, ${position.y}px) translate(-50%, -50%)`
+      }}
+    >
+      {type && `${type.charAt(0).toUpperCase() + type.slice(1)} Node`}
+    </div>
+  );
+}
+
+
+export default memo(SidebarEdit)
